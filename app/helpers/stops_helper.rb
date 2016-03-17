58e4ce4e8aaadba289eb098ef04cbdad27debc0e
vbhjckfd <vbhjckfd@gmail.com>
2016-03-17 11:19:01 +0200

    url fix

--- app/helpers/stops_helper.rb
+++ app/helpers/stops_helper.rb
@@ -23,4 +23,13 @@ module StopsHelper
     disp_time.strftime("%-M хв")
   end
 
+  def stop_path(stop, options={})
+    stop_url(stop, options)
+  end
+
+  def stop_url(stop, options={})
+    url_for(options.merge(:controller => 'stops', :action => 'show',
+                          :id => stop.code))
+  end
+
 end
