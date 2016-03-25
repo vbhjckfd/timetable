90af0eeb65ed899e01ca9fd868c6aea5cbc8b858
vbhjckfd <vbhjckfd@gmail.com>
2016-03-25 16:06:11 +0200

    Add transport location to API

--- app/controllers/api/stop_controller.rb
+++ app/controllers/api/stop_controller.rb
@@ -49,7 +49,15 @@ class Api::StopController < ApplicationController
       else
         :bus
       end
-      @response << {route: strip_route(item["RouteName"]), vehicle_type: vehicle_type, end_stop: item['IterationEnd'], seconds_left: item["TimeToPoint"], time_left: round_time(item["TimeToPoint"])}
+      @response << {
+        route: strip_route(item["RouteName"]), 
+        vehicle_type: vehicle_type, 
+        end_stop: item['IterationEnd'],
+        seconds_left: item["TimeToPoint"], 
+        time_left: round_time(item["TimeToPoint"]),
+        longitude: item['X'],
+        latitude: item['Y'],
+      }
     end
 
     case params[:format]
