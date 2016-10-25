8c9daed54ecc1667bb0c5cab3dc5ff1df2fe273a
vbhjckfd <vbhjckfd@gmail.com>
2016-10-25 16:41:06 +0300

    Route fix

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -9,7 +9,7 @@ class StopsController < ApplicationController
   # GET /stops/1
   # GET /stops/1.json
   def show
-    redirect_to action: "show", id: params[:id].sub(/^[0:]*/,""), status: :moved_permanently if params[:id].start_with?('0')
+    params[:id].sub!(/^[0:]*/,"") if params[:id].start_with?('0')
 
     @stop = Stop.find_by(code: params[:id])
   end
