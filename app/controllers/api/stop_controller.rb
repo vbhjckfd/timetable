79b860fc0a4e8f1f2dfde4974c32fd47d14b90fa
vbhjckfd <vbhjckfd@gmail.com>
2016-12-20 18:15:26 +0200

    Fix

--- app/controllers/api/stop_controller.rb
+++ app/controllers/api/stop_controller.rb
@@ -8,7 +8,7 @@ class Api::StopController < ApplicationController
 
     return render(status: :bad_request, text: "No stop with code #{stop_id}") unless stop
 
-    timetable = Rails.cache.fetch("stop_timetable/#{stop_id}", expires_in: 15.seconds) do
+    timetable = Rails.cache.fetch("stop_timetable/#{stop_id}", expires_in: 30.seconds) do
       stop.get_timetable
     end
     response = stop.as_json.merge timetable: timetable || []
