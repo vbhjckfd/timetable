2f847d985ee875eead89e5419e95104254a7b1ca
vbhjckfd <vbhjckfd@gmail.com>
2017-01-03 12:19:47 +0200

    Make rendering static

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -9,9 +9,7 @@ class StopsController < ApplicationController
   # GET /stops/1
   # GET /stops/1.json
   def show
-    params[:id].sub!(/^[0:]*/,"") if params[:id].start_with?('0')
 
-    @stop = Stop.find_by(code: params[:id])
   end
 
   def closest
