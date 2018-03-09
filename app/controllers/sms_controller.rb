b3dcfb54a96e9646d7ffab4e50eadb02271e913e
vbhjckfd <vbhjckfd@gmail.com>
2018-03-09 17:49:55 +0200

    Fix in sms controller

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,5 +1,9 @@
 class SmsController < ApplicationController
 
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :null_session
+
   def get
     xml_doc  = Nokogiri::XML(request.raw_post).remove_namespaces!
     stop_code = xml_doc.xpath('//body').text
@@ -34,7 +38,7 @@ class SmsController < ApplicationController
       x[0..3] <=> y[0..3]
     end
 
-    builder = Nokogiri::XML::Builder.new do |xml|
+    builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
       xml.answer(:type => 'sync') {
         xml.body(:paid => false) {
           xml.cdata(result.join "\n")
