b85d71fe4ffb1f6a91d553d0fe9d2741704b9ca3
vbhjckfd <vbhjckfd@gmail.com>
2018-04-18 18:02:56 +0300

    Temporary disable time caching

--- app/controllers/api/stop_controller.rb
+++ app/controllers/api/stop_controller.rb
@@ -8,9 +8,10 @@ class Api::StopController < ApplicationController
 
     return render(status: :bad_request, text: "No stop with code #{stop_id}") unless stop
 
-    timetable = Rails.cache.fetch("stop_timetable/#{stop_id}", expires_in: 15.seconds) do
-      stop.get_timetable
-    end
+    # timetable = Rails.cache.fetch("stop_timetable/#{stop_id}", expires_in: 15.seconds) do
+    #   stop.get_timetable
+    # end
+    timetable = stop.get_timetable
     response = stop.as_json.symbolize_keys.slice(:name, :longitude, :latitude, :code).merge timetable: timetable || []
 
     response[:routes] = Route.through(stop).map{|r| r.name }
